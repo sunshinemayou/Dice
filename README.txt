0ab33167e056c8bb72fd85d04ea8be241a673d16
Maymay <maymay4ever@163.com>
2014-11-07 09:14:14 +0800

    This is a very first start, the computer random choose the number

---
+++ README.txt
@@ -0,0 +1,2 @@
+This program is used to stimulate a dice to roll
+This README.txt is used to describing its behavior
---
+++ dice.py
@@ -0,0 +1,5 @@
+#! usr/bin/env python
+
+import random
+number = random.randint(1,6)
+print("The dice show:" + str(number))
