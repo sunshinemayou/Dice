d97a6329ea5b9f859aa30f4ab4ae32cd0b047d00
Maymay <maymay4ever@163.com>
2014-11-07 09:38:40 +0800

    Add the question to ask how many sides there are on the dice

--- dice.py
+++ dice.py
@@ -1,5 +1,7 @@
 #! usr/bin/env python
 
 import random
+import specialInput
+sides = specialInout.int_input("How many sides on your dice?")
 number = random.randint(1,6)
 print("The dice show:" + str(number))
