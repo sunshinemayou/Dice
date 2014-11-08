40670e72d271724d75b2dc1e1a4c34cf028cc811
Maymay <maymay4ever@163.com>
2014-11-08 22:57:29 +0800

    Show the number of dice

--- dice.py
+++ dice.py
@@ -2,8 +2,17 @@
 
 import random
 import specialInput
+
 dice = specialInput.int_input("How many dice would you like to roll? ")
-sides = specialInput.int_input("How many sides on your dice?")
+
 for i in range(1, dice+1):
-	number = random.randint(1, sides)
-	print("dice" + str(i) + " shows " +str(number))
+
+	sides = specialInput.int_input("How many sides on dice #{}? ".format(i))
+	if sides > 0:
+		number = random.randint(1, sides+1)
+	else:
+		print("Sorry dude, I think you made a mistake")
+	print("Dice" + " " + str(i) + " shows: " + str(number))
+	
+	
+	
