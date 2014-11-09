ce37b87f13d30b21c1874801a1e67fcd815919d5
Maymay <maymay4ever@163.com>
2014-11-09 18:08:07 +0800

    Fix docstrings and add instructions

--- dice.py
+++ dice.py
@@ -2,15 +2,14 @@
 
 import random
 import specialInput
-"""
-	Asking the user to input how many dices they would like to use
-"""
+
+print("This program simulates rolling several dice.\nThe user can choose how many dice are rolled")
+
+# Asking the user to input how many dices they would like to use
+
 dice = specialInput.int_input("How many dice would you like to roll? ")
 
 for i in range(1, dice+1):
-	"""
-		Asking the user to input the sides of each dice
-	"""
 	sides = specialInput.int_input("How many sides on dice #{}?".format(i))
 	if sides <= 2:
 		print("Each die has at least two sides")
@@ -18,13 +17,7 @@ for i in range(1, dice+1):
 	elif sides > 2:
 		number = random.randint(1, sides+1)
 	else:
-		"""
-			If you see this sentence, it means that you have made a mistake when inputting the side of this dice
-		"""	
 		print("Sorry dude, I think you made a mistake")
-		"""
-			This is an output language
-		"""
 	print("Dice" + " " + str(i) + " shows: " + str(number))
 	
 	
